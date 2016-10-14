92e43905292c2623ee4e2ba82487c7b037d0b28b
Tudor M. Pristavu <clapautius@users.noreply.github.com>
2016-10-14 19:09:39 +0300

    the-accountant: add real params.h.

--- bots/the-accountant/params.h
+++ bots/the-accountant/params.h
@@ -1,30 +1,33 @@
 #ifndef PARAMS_H
 #define PARAMS_H
 
-static const int k_standard_step = 0;
+static const int k_standard_step = 1000;
 
-static const int k_recommended_distance_more = 0;
-static const int k_recommended_distance_many_more = 0;
-static const int k_recommended_distance_one = 0;
-static const int k_recommended_distance_strong = 0;
-static const double k_recommended_factor = 1;
+static const int k_recommended_distance_more = 5500;
+static const int k_recommended_distance_many_more = 6500; // prev + 1000
+static const int k_recommended_distance_one = 5500;
+static const int k_recommended_distance_strong = 3800;
+static const double k_recommended_factor = 1.4;
+static const int k_recommended_dist_radius_around = 6500;
 
-static const int k_min_distance = 0;
-static const int k_safe_distance = 0;
-static const int k_enemy_distance = 0;
+static const int k_min_distance = 2100;
+static const int k_safe_distance = 2600;
+static const int k_enemy_distance = 3500;
 
-/// determine if an enemy is 'alone'
-static const int k_distance_between_enemies = 0;
-
-static const int k_runaway_step = 0;
-static const int k_runaway_step_diag = k_runaway_step / 1;
+static const int k_runaway_step = 1000;
+static const int k_runaway_step_diag = k_runaway_step / 1.4142;
+static const int k_runaway_safe_dist_to_point = 800;
+static const int k_runaway_small_angle = 15;
 
 /// just some big value, bigger than any possible distance
-static const int k_max_dist = 0;
+static const int k_max_dist = 100000;
 
-static const int k_safe_angle = 0;
-static const int k_safe_dist_me_from_point = 0;
-static const int k_safe_dist_enemy_from_point = 0;
-static const int k_safe_dist_me_from_enemy_away = 0;
+static const int k_safe_angle = 70;
+static const int k_safe_dist_me_from_point = 1500;
+static const int k_safe_dist_enemy_from_point = 1500;
+static const int k_safe_dist_me_from_enemy_away = 1500;
+
+static const int k_max_enemies_to_proc = 30;
+static const int k_max_points_to_proc = 60;
 
 #endif
