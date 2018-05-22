ebb1a58865e26d75bcdebf4a9626b785cd9f87be
Tudor M. Pristavu <clapautius@users.noreply.github.com>
2018-05-22 20:36:07 +0300

    temperatures-code-golf: try2 (159 chars).

--- code-golf/temperatures-code-golf.cc
+++ code-golf/temperatures-code-golf.cc
@@ -1,11 +1,3 @@
 #include <iostream>
-using namespace std;
-int main()
-{int i,r=0,s=5527,v,a;cin>>i;
-if(i)do{
-cin>>v;
-a=(v>0?v:-1*v);
-if(a<s)r=v,s=a;
-if(r==-1*v)r=s;
-}while(cin.good());
-cout<<r<<"\n";}
+#define C std::cin
+int main(){int r=0,s=5527,v,a;C>>v;if(v)do{C>>v;a=(v>0?v:-v);a<s&&(r=v,s=a);r==-v&&(r=s);}while(C.good());std::cout<<r;}
