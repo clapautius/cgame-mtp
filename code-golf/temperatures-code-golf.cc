64120ea8e4c18a335f9104a24b8dd02f0e037e8b
Tudor M. Pristavu <clapautius@users.noreply.github.com>
2018-05-23 20:23:50 +0300

    temperatures-code-golf: try 4 (143 chars).

--- code-golf/temperatures-code-golf.cc
+++ code-golf/temperatures-code-golf.cc
@@ -1,3 +1,3 @@
 #include <iostream>
 #define C std::cin
-int main(){int r=0,s=5527,v,a;C>>a;for(C>>v;a&&C;a=(v>0?v:-v),a<s&&(r=v,s=a),r==-v&&(r=s),C>>v);std::cout<<r;}
+main(){int r=0,s=5527,v,a;C>>a;for(C>>v;a&&C;a=v>0?v:-v,a<s&&(r=v,s=a),r==-v&&(r=s),C>>v);std::cout<<r;}
