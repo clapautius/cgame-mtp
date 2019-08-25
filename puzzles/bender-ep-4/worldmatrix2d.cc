31dc63ece127f1025a63de8f6d294fc1ecb52109
Tudor M. Pristavu <clapautius@users.noreply.github.com>
2019-08-25 21:44:05 +0300

    bender-ep-4: working on WorldMatrix2D.

--- puzzles/bender-ep-4/worldmatrix2d.cc
+++ puzzles/bender-ep-4/worldmatrix2d.cc
@@ -1,6 +1,7 @@
 #include "worldmatrix2d.h"
 
 using std::vector;
+using cgame::Location;
 
 WorldMatrix2D::WorldMatrix2D(unsigned width, unsigned height)
   : m_width(width), m_height(height)
@@ -28,4 +29,37 @@ void WorldMatrix2D::set_at(unsigned x, unsigned y, int value)
     if (!(x < 0 || y < 0 || x > (m_width - 1) || y > (m_height -1))) {
         m_world_matrix[y][x] = value;
     }
+}
+
+
+void WorldMatrix2D::fill(int value)
+{
+    for (unsigned i = 0; i < m_height; i++)
+        for (unsigned j = 0; j < m_width; j++)
+            m_world_matrix[i][j] = value;
+}
+
+
+void WorldMatrix2D::find_path_dfs_between_rec(
+  std::function<bool (Location)> is_location_free,
+  const Location &start, const Location &end, int max_depth,
+  WorldMatrix2D &helper_matrix, vector<Location> &path)
+{
+
+}
+
+
+std::vector<Location> WorldMatrix2D::find_path_dfs_between(
+  unsigned width, unsigned height,
+  std::function<bool (Location)> is_location_free,
+  const Location &start, const Location &end, int max_depth)
+{
+    vector<Location> ret;
+    // check if the start position is valid
+    if (!location_valid_p(start, width, height)) {
+        return ret;
+    }
+    WorldMatrix2D helper_matrix(width, height);
+    helper_matrix.fill(-1);
+
 }
--- puzzles/bender-ep-4/worldmatrix2d.h
+++ puzzles/bender-ep-4/worldmatrix2d.h
@@ -32,6 +32,25 @@ public:
      */
     void set_at(unsigned x, unsigned y, int value);
 
+    /**
+     * @todo :fixme: add this to a special constructor.
+     */
+    void fill(int value);
+
+    /**
+     * @return the path between start and end or an empty vector if no such path was
+     * found.
+     */
+    static std::vector<cgame::Location> find_path_dfs_between(
+      unsigned width, unsigned height,
+      std::function<bool (cgame::Location)> is_location_free,
+      const cgame::Location &start, const cgame::Location &end, int max_depth);
+
+    static bool location_valid_p(const cgame::Location &loc, unsigned width, unsigned height)
+    {
+        return (loc.x() >= 0 && loc.x() < width && loc.y() >= 0 && loc.y() < height);
+    }
+
 private:
 
     std::vector<std::vector<int>> m_world_matrix;
@@ -39,6 +58,12 @@ private:
     unsigned m_width;
 
     unsigned m_height;
+
+    void find_path_dfs_between_rec(
+      std::function<bool (cgame::Location)> is_location_free,
+      const cgame::Location &start, const cgame::Location &end, int max_depth,
+      WorldMatrix2D &helper_matrix, std::vector<cgame::Location> &path);
+
 };
 
 #endif
